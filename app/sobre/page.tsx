b686c9fb281878e4d7da73416c9ef40c928e1aaa
Erick Rosa <erickpmotta@gmail.com>
2024-03-20 23:57:04 -0300

    update about

--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -6,32 +6,34 @@ export default async function About() {
           Sobre
         </h1>
         <p className="leading-7 [&:not(:first-child)]:mt-6">
-          Patrol Analytics é uma iniciativa aberta e colaborativa que oferece
-          visualizações e análises avançadas sobre ocorrências de violência
-          armada no Rio de Janeiro. Este projeto foi criado para fornecer um
-          entendimento mais aprofundado e objetivo dos incidentes de segurança
-          pública, utilizando como base a maior base de dados sobre violência
-          armada da América Latina, fornecida pela API do Fogo Cruzado.
+          Patrol Analytics é uma iniciativa aberta e colaborativa dedicada a
+          fornecer visualizações e análises avançadas das ocorrências de
+          violência armada no Rio de Janeiro. Criado para propiciar um
+          entendimento mais aprofundado e objetivo sobre os incidentes de
+          segurança pública, este projeto se fundamenta na mais extensa base de
+          dados sobre violência armada da América Latina, disponibilizada pela
+          API do Fogo Cruzado.
         </p>
 
         <p className="leading-7 [&:not(:first-child)]:mt-6">
-          A ideia base do projeto é servir como uma ferramenta para cidadãos,
-          acadêmicos e instituições que buscam compreender e atuar na realidade
-          da violência urbana. As funcionalidades incluem a capacidade de
-          filtrar dados por data, localidade e tipo de incidente, além de exibir
-          estatísticas resumidas, detalhes sobre os incidentes mais recentes,
-          mapas interativos e a possibilidade de exportar dados para análise
-          mais aprofundada. O projeto também está em constante evolução, com
-          novas funcionalidades e melhorias sendo adicionadas regularmente.
+          Com o propósito de servir como uma ferramenta para cidadãos,
+          acadêmicos e instituições interessadas em compreender e atuar na
+          realidade da violência urbana, o projeto oferece recursos como
+          filtragem de dados por data, localidade e tipo de incidente. Além
+          disso, apresenta estatísticas condensadas, informações detalhadas
+          sobre os incidentes mais recentes, mapas interativos e a opção de
+          exportar dados para análises mais detalhadas. O projeto também está em
+          constante evolução, com novas funcionalidades e melhorias sendo
+          adicionadas regularmente.
         </p>
 
         <p className="leading-7 [&:not(:first-child)]:mt-6">
           Este é um projeto vivo, construído em público e para o público.
-          Convidamos a comunidade a contribuir e apoiar de
-          diversas formas: através de desenvolvimento de código, feedbacks
-          construtivos e etc. Para fazer parte dessa iniciativa, visite
-          nosso repositório no GitHub, contribua com o projeto ou entre em
-          contato para discutir outras maneiras de apoiar o projeto.
+          Convidamos a comunidade a contribuir e apoiar de diversas formas:
+          através de desenvolvimento de código, feedbacks construtivos e etc.
+          Para fazer parte dessa iniciativa, visite nosso repositório no GitHub,
+          contribua com o projeto ou entre em contato para discutir outras
+          maneiras de apoiar o projeto.
         </p>
       </div>
 
